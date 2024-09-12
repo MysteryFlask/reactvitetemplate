2c1074ac2d94e16546bd3c0bd1b7af12b908dc33
MysteryFlask <mazzua24@gmail.com>
2024-09-12 15:14:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Vite Template
+
+This is my custom template to create a new React project with Vite.
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './style.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='title'>Template</h1>
+    </>
+  )
+}
+
+export default App
